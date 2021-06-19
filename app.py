e169453c2dfaff4f154e7ac2cfbbe0a969998c3f
Bobby Gajbhiye <bobbygajbhiye@ymail.com>
2021-06-19 14:41:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from sklearn.preprocessing import StandardScaler
 import pickle
 
 model=pickle.load(open('SUV_model.pkl','rb'))
@@ -9,7 +10,7 @@ def post_fun():
     gender=int(request.form['gen'])
     age=int(request.form['a'])
     salary=int(request.form['sal'])
-    pred=model.predict([[gender,age,salary]])
+    pred=model.predict(StandardScaler().fit_transform([[gender,age,salary]]))
     return render_template('output.html',prediction=pred)
 
 @app.route('/')
